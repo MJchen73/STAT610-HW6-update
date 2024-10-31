24e0c223570c31d4f33e8b9738092467902c5085
Mujia Chen <mj@chenmujiadeMacBook-Air.local>
2024-10-30 21:27:12 -0400

    Optimize llr function to use weight vector instead of matrix (speed-test-1)

--- llr_functions.R
+++ llr_functions.R
@@ -1,18 +1,14 @@
-# llr_functions.R (优化 1：在 speed-test-1 分支中)
+# llr_functions.R (优化版本：speed-test-1)
 
 # llr function that computes the fits at a point zi and apply it to each element of z:
 llr = function(x, y, z, omega) {
-  # 使用 sapply 对每个 z 进行计算
   fits = sapply(z, compute_f_hat, x, y, omega)
   return(fits)
 }
 
 # Compute f hat function (优化 1):
 compute_f_hat = function(z, x, y, omega) {
-  # 获取权重向量而不是对角矩阵
-  weights = make_weight_vector(z, x, omega)
-  
-  # 创建预测矩阵 X
+  weights = make_weight_vector(z, x, omega)  # 改为权重向量
   X = make_predictor_matrix(x)
   
   # 使用权重向量与矩阵逐元素相乘，而不是使用矩阵乘法
@@ -20,7 +16,7 @@ compute_f_hat = function(z, x, y, omega) {
   Wy = y * weights  # 对 y 进行加权
   
   # 计算拟合值
-  f_hat = c(1, z) %*% solve(t(WX) %*% X) %*% t(WX) %*% y
+  f_hat = c(1, z) %*% solve(t(WX) %*% X) %*% t(WX) %*% Wy
   
   return(f_hat)
 }
